362954e2e8d3f124a9ea802d67399629f303e6f7
Cutler <cutlerthayer@gmail.com>
2020-02-19 13:49:23 -0600

    Adding buttons

--- Cutlers_Experiments/Hangman/src/userInterface/SwingExample.java
+++ Cutlers_Experiments/Hangman/src/userInterface/SwingExample.java
@@ -1,6 +1,21 @@
 package userInterface;
 import javax.swing.*;
+import javax.swing.JButton;
 
 public class SwingExample {
 
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new MainFrame("Hangman Example Window");
+				JButton b = new JButton("Letter");
+				b.setBounds(10, 10, 10, 10);
+				frame.add(b);
+				frame.setSize(500, 400);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+		
+	}
 }
